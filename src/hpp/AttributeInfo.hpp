ed883ca1b3300c9e6627590d4e1e600130254e5d
Igor Figueira Pinheiro da Silva <igorfgds@gmail.com>
2019-05-13 21:09:01 -0300

    Finished AttributeInfo prototype

--- src/hpp/AttributeInfo.hpp
+++ src/hpp/AttributeInfo.hpp
@@ -6,45 +6,101 @@
 
 using namespace std;
 
-class ConstantValue {
+class ConstantValueAttribute {
 private:
     uint16_t constantValueIndex;
+public:
+    void read(FILE*);
+    void print(vector<CPInfo>);
 };
 
-// class Code {
-// private:
-//     uint16_t max_stack;
-//     uint16_t max_locals;
-//     uint32_t code_length;
-//     uint8_t *code;
-//     uint16_t exception_table_length;
-//     union {
-//     uint16_t start_pc;
-//     uint16_t end_pc;
-//     uint16_t handler_pc;
-//     uint16_t catch_type;
-//     } exception_table[exception_table_length];
-//     uint16_t attributes_count;
-//     vector<AttributeInfo> attributes;
-// };
+class CodeAttribute {
+private:
+    class ExceptionHandler {
+    private:
+        uint16_t start_pc;
+        uint16_t end_pc;
+        uint16_t handler_pc;
+        uint16_t catch_type;
+    };
 
+    uint16_t maxStack;
+    uint16_t maxLocals;
+    uint32_t codeLength;
+    vector<uint8_t> code;
+    uint16_t exceptionTableLength;
+    vector<ExceptionHandler>exceptionTable;
+    uint16_t attributesCount;
+    vector<AttributeInfo> attributes;
+public:
+    void read(FILE*, vector<CPInfo>);
+    void print(vector<CPInfo>);
+};
+
+class ExceptionsAttribute {
+private:
+    uint16_t numberOfExceptions;
+    vector<uint16_t> exceptionIndexTable;
+public:
+    void read(FILE *);
+    void print(std::vector<CPInfo>);
+};
+
+class InnerClassesAttribute {
+private:
+    class ClassInfo {
+    private:
+        uint16_t innerClassInfoIndex;
+        uint16_t outerClassInfoIndex;
+        uint16_t innerNameIndex;
+        uint16_t innerClassAccessFlags;
+    };
+
+    uint16_t numberOfClasses;
+    vector<ClassInfo> classes;
+public:
+    void read(FILE*);
+    void print(vector<CPInfo>);
+};
+
+class SourceFileAttribute {
+private:
+    uint16_t sourcefileIndex;
+public:
+    void read(FILE*);
+    void print(vector<CPInfo>);
+};
+
+class LineNumberTableAttribute {
+private:
+    class LineNumber {
+        uint16_t startPC;
+        uint16_t lineNumber;
+    };
+
+    uint16_t lineNumberTableLength;
+    vector<LineNumber> lineNumberTable;
+public:
+    void read(FILE*);
+    void print(vector<CPInfo>);
+};
 
 class AttributeInfo {
 private:
     uint16_t attributeNameIndex;
     uint32_t attributeLength;
     union {
-        ConstantValue constantValue;
-        Code code;
+        ConstantValueAttribute constantValue;
+        CodeAttribute code;
         // StackMapTable stackMapTable;
-        Exceptions exceptions;
-        InnerClasses innerClasses;
+        ExceptionsAttribute exceptions;
+        InnerClassesAttribute innerClasses;
         // EnclosingMethod enclosingMethod;
         // Synthetic synthetic;
         // Signature signature;
-        SourceFile sourceFile;
+        SourceFileAttribute sourceFile;
         // SourceDebugExtension sourceDebugExtension;
-        LineNumberTable lineNumberTable;
+        LineNumberTableAttribute lineNumberTable;
         // LocalVariableTable localVariableTable;
         // LocalVariableTypeTable localVariableTypeTable;
         // Deprecated deprecated;
@@ -57,7 +113,9 @@ private:
         // AnnotationDefault annotationDefault;
         // BootstrapMethods bootstrapMethods;
         // MethodParameters methodParameters;
-    }
+    };
+public:
+    ~AttributeInfo();
 };
 
 #endif
